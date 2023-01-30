a0dd42fc9bbe4d1c0b55890bc62780cc51a770a9
mamoudou <konemamoudou84@gmail.com>
2023-01-30 02:44:24 +0100

    :memo: ajout de modif sur le shufflecard

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const board = document.querySelector("#board");
 
+// creer le tableaux  du jeu
 const createBoard = (board) => {
+
     for (let i = 0; i < 1; i++) {
         let ul = document.createElement('ul');
         ul.classList.add('cards')
@@ -33,13 +35,31 @@ const createBoard = (board) => {
 }
 createBoard(board);
 
-
+// select toute les cartes 
 const cards = document.querySelectorAll('.card');
-let matching = 0;
+const reset = document.querySelector('.reset');
+const score = document.querySelector('.score');
+
+let countScore = 0;
+let matchCard = 0;
 let disckCheck = false;
 let cardOne, cardTwo;
 
+reset.addEventListener('click',()=>{
+    countScore = 0;
+    score.innerText = "Votre score"
+    cardshuffle();
+});
+
+/**
+ * fonction qui retourne les carte
+ * qui recupere les images de la carte une et deux
+ * elle prends aussi la functon match qui verifie les cartes identique
+ * @param {*} el 
+ * @returns 
+ */
 function flipcard(el) {
+
     let checked = el.target;
 
     if (checked !== cardOne && !disckCheck) {
@@ -61,9 +81,27 @@ function flipcard(el) {
     matchImg(imgOne, imgTwo);
 }
 
+/**
+ * cette fonction vérifie si les images sont identiques 
+ * @param {*} img1 
+ * @param {*} img2 
+ * @returns 
+ */
+
 function matchImg(img1, img2) {
 
     if (img1 === img2) {
+
+        matchCard++
+        if(matchCard === 8 ) {
+            countScore++;
+            score.innerText = `Votre score ${countScore}`;
+            console.log(countScore);
+            setTimeout(() => {
+                cardshuffle();
+            }, 900);
+
+        }
 
         cardOne.removeEventListener('click', flipcard);
         cardTwo.removeEventListener('click', flipcard);
@@ -77,7 +115,7 @@ function matchImg(img1, img2) {
         setTimeout(() => {
             cardOne.classList.add('vibration');
             cardTwo.classList.add('vibration');
-        }, 300);
+        }, 200);
 
         setTimeout(() => {
             cardOne.classList.remove('vibration', 'flip');
@@ -86,10 +124,41 @@ function matchImg(img1, img2) {
             cardOne = cardTwo = "";
             return disckCheck = false;
 
-        }, 1000);
+        }, 700);
     }
 }
 
+/**
+ * function qui melange les cartes 
+ */
+
+function cardshuffle(){
+
+    cardOne = cardTwo = "";
+    disckCheck = false;
+    matchCard = 0;
+    
+    let array = [1, 2, 3, 4, 5,6, 7, 8, 1, 2, 3, 4, 5,6, 7, 8];
+
+    array.sort(()=>(Math.random() > 0.5 ? 1 : -1));
+
+    console.log(array)
+
+    cards.forEach((card, index ) => {
+        card.classList.remove('flip');
+
+        let tagImg = card.querySelector('.view-back img');
+        tagImg.src = `img/image-${array[index]}.png`;
+        console.log(tagImg.src)
+
+        card.addEventListener('click', flipcard);
+    })
+}
+
+cardshuffle();
+
 cards.forEach(card => {
-    card.addEventListener('click', flipcard)
+    /*  card.classList.add('flip') */
+   
+card.addEventListener('click', flipcard);
 })
